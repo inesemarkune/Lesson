5334d537a3b12f9707b7c09fa94895474b3b0472
Inese Markune <inese.markune@gmail.com>
2020-03-09 19:29:19 +0200

    added printline Hello Boys

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,6 +3,9 @@ public class FirstProgram {
 
 		public static void main(String args[]){
 				System.out.println("Hello Girls!");
+				System.out.println("Hello Boys!");
+				
+				
 		}//end main
 		
 }//end class
